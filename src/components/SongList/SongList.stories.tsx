e6023e10030ca308d4936d3332bb69f541876d86
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2022-08-03 17:54:42 +0200

    fix SongList

--- src/components/SongList/SongList.stories.tsx
+++ src/components/SongList/SongList.stories.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Song } from "../../types";
 import { SongList } from "./SongList";
 
 export default {
   title: "Components/SongList",
   component: SongList,
 };
-const client = new QueryClient();
 
 export const Default = () => {
+  const [songs, setSongs] = React.useState<Song[]>([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const isError = false;
+
+  const askForASong = React.useCallback(async () => {
+    setIsLoading(true);
+    // wait for 0.5 second
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    setSongs([
+      ...songs,
+      {
+        id: "1",
+        title: "New song",
+        artist: "New artist",
+        filename: "New filename",
+        rating: 0,
+      },
+    ]);
+    setIsLoading(false);
+  }, [songs]);
+
   return (
-    <QueryClientProvider client={client}>
-      <SongList setSelected={() => {}} onAdd={() => {}} />;
-    </QueryClientProvider>
+    <SongList
+      setSelected={(song) =>
+        song && alert("A Song has been selected: " + song.title)
+      }
+      onAdd={() => askForASong()}
+      fetchedSongs={{ songs, isLoading, isError }}
+    />
   );
 };
